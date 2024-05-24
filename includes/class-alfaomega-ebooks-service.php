baf7a189286e9e01b7068ec9b70721282db8b971
Livan Rodriguez <livan2r@gmail.com>
2024-05-24 14:06:15 -0400

    todo list updated

--- includes/class-alfaomega-ebooks-service.php
+++ includes/class-alfaomega-ebooks-service.php
@@ -13,6 +13,10 @@ if( ! class_exists( 'Alfaomega_Ebooks_Service' )){
 
         public function importEbooks(): array
         {
+            // pull from panel all new ebooks
+            // add each new ebook
+            // link each related product
+
             return [
                 'imported' => rand(0, 2)
             ];
@@ -20,6 +24,10 @@ if( ! class_exists( 'Alfaomega_Ebooks_Service' )){
 
         public function refreshEbooks(): array
         {
+            // pull from panel the information of each ebook already imported
+            // update or create the ebook information
+            // refresh the link of each related product
+
             return [
                 'refreshed' => rand(0, 2)
             ];
@@ -27,6 +35,10 @@ if( ! class_exists( 'Alfaomega_Ebooks_Service' )){
 
         public function linkProducts(): array
         {
+            // pull the eBook information of each linked product
+            // update or create the eBook information
+            // refresh de link of each product
+
             return [
                 'linked' => rand(0, 2)
             ];
