1960de5b4562197b9f0040b928bc20cf97961771
Livan Rodriguez <livan2r@gmail.com>
2024-06-10 09:00:29 -0400

    docs update

--- admin/class-alfaomega-ebooks-wc-settings.php
+++ admin/class-alfaomega-ebooks-wc-settings.php
@@ -1,28 +1,22 @@
 <?php
-/**
- * Extends the WC_Settings_Page class
- *
- * @link        https://paulmiller3000.com
- * @since       1.0.0
- *
- * @package     P3k_Galactica
- * @subpackage  P3k_Galactica/admin
- *
- */
-
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+// Check if the class AlfaomegaEbooks_WC_Settings exists
 if ( ! class_exists( 'AlfaomegaEbooks_WC_Settings' ) ) {
 
     /**
-     * Settings class
+     * The AlfaomegaEbooks_WC_Settings class extends the WC_Settings_Page class.
+     * It provides the settings for the Alfaomega Ebooks plugin in the WooCommerce settings.
      *
      * @since 1.0.0
      */
     class AlfaomegaEbooks_WC_Settings extends WC_Settings_Page {
 
         /**
-         * Constructor
+         * Constructor for the AlfaomegaEbooks_WC_Settings class.
+         * It initializes the parent class and sets the id and label properties.
+         * It also adds the necessary hooks for the settings page.
+         *
          * @since  1.0
          */
         public function __construct() {
@@ -30,19 +24,18 @@ if ( ! class_exists( 'AlfaomegaEbooks_WC_Settings' ) ) {
             $this->id    = 'alfaomega-ebooks';
             $this->label = esc_html__( 'Alfaomega Ebooks', 'alfaomega-ebooks' );
 
-            // Define all hooks instead of inheriting from parent                    
+            // Define all hooks instead of inheriting from parent
             add_filter( 'woocommerce_settings_tabs_array', array( $this, 'add_settings_page' ), 20 );
             add_action( 'woocommerce_sections_' . $this->id, array( $this, 'output_sections' ) );
             add_action( 'woocommerce_settings_' . $this->id, array( $this, 'output' ) );
             add_action( 'woocommerce_settings_save_' . $this->id, array( $this, 'save' ) );
-            
+
         }
 
-
         /**
-         * Get sections.
+         * Get the sections for the settings page.
          *
-         * @return array
+         * @return array The sections for the settings page.
          */
         public function get_sections() {
             $sections = array(
@@ -54,33 +47,32 @@ if ( ! class_exists( 'AlfaomegaEbooks_WC_Settings' ) ) {
             return apply_filters( 'woocommerce_get_sections_' . $this->id, $sections );
         }
 
-
         /**
-         * Get settings array
+         * Get the settings for the current section.
          *
-         * @return array
+         * @return array The settings for the current section.
          */
         public function get_settings() {
 
             global $current_section;
             $prefix = 'alfaomega_ebooks_';
             $settings = array();
-            
+
             switch ($current_section) {
                 case 'log':
-                    $settings = array(                              
+                    $settings = array(
                             array()
                     );
                     break;
                 default:
                     include 'partials/alfaomega-ebooks-settings-main.php';
-            }   
+            }
 
-            return apply_filters( 'woocommerce_get_settings_' . $this->id, $settings, $current_section );                   
+            return apply_filters( 'woocommerce_get_settings_' . $this->id, $settings, $current_section );
         }
 
         /**
-         * Output the settings
+         * Output the settings for the current section.
          */
         public function output() {
             global $current_section;
@@ -92,16 +84,16 @@ if ( ! class_exists( 'AlfaomegaEbooks_WC_Settings' ) ) {
                 default:
                     $settings = $this->get_settings();
                     WC_Admin_Settings::output_fields( $settings );
-            }               
-            
+            }
+
         }
 
         /**
-         * Save settings
+         * Save the settings for the current section.
          *
          * @since 1.0
          */
-        public function save() {                    
+        public function save() {
             $settings = $this->get_settings();
 
             WC_Admin_Settings::save_fields( $settings );
@@ -111,5 +103,5 @@ if ( ! class_exists( 'AlfaomegaEbooks_WC_Settings' ) ) {
 
 }
 
-
+// Return a new instance of the AlfaomegaEbooks_WC_Settings class
 return new AlfaomegaEbooks_WC_Settings();
