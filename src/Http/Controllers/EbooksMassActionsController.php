3b4805bd97feaae50d5c26f7fe674ec2f44b5fa3
Livan Rodriguez <livan2r@gmail.com>
2024-06-13 15:24:09 -0400

    working on mass actions

--- src/Http/Controllers/EbooksMassActionsController.php
+++ src/Http/Controllers/EbooksMassActionsController.php
@@ -3,116 +3,101 @@
 namespace AlfaomegaEbooks\Http\Controllers;
 
 use AlfaomegaEbooks\Services\eBooks\Service;
-use WP_REST_Request;
-use WP_REST_Response;
+use Exception;
 
 class EbooksMassActionsController
 {
     /**
-     * Import eBooks from Alfaomega.
+     * Performs a mass update of metadata for a set of eBooks.
      *
-     * This method is responsible for importing eBooks from Alfaomega.
-     * It uses the eBooks service to import eBooks and returns a response with the status and message.
+     * This method takes an array of post IDs and a redirect URL as parameters. It attempts to refresh the eBook metadata
+     * for each post ID in the array by calling the `refreshEbook` method on the eBooks service. The number of eBooks
+     * successfully updated is then added to the redirect URL as a query parameter.
      *
-     * @return WP_REST_Response The response containing the status and message.
+     * If an exception occurs during the update process, the exception message is logged using the `error_log` function,
+     * and 'fail' is added to the redirect URL as a query parameter.
+     *
+     * @param array $postIds An array of post IDs for which to update the eBook metadata.
+     * @param string $redirectUrl The URL to which the user should be redirected after the update process is complete.
+     * @return string The redirect URL with the result of the update process added as a query parameter.
+     * @throws Exception If an error occurs during the update process.
      */
-    public function masImportEbooks(array $postIds): string
+    public function masUpdateMeta(array $postIds, string $redirectUrl): string
     {
         try {
-            $response = Service::make()->ebooks()
-                ->importEbook()
-                ->batch();
-
-            $message = $response['imported'] > 0
-                ? str_replace('%s', $response['imported'], esc_html__("%s new ebooks were added to the import scheduler successfully!", 'alfaomega-ebooks'))
-                : esc_html__('No new eBooks to import', 'alfaomega-ebooks');
-
-            return new WP_REST_Response([
-                    'status'  => 'success',
-                    'data'    => $response,
-                    'message' => $message,
-                ], 200);
-        } catch (\Exception $exception) {
-            return new WP_REST_Response([
-                'status'  => 'fail',
-                'message' => $exception->getMessage()
-            ], 400);
+            $redirectUrl = remove_query_arg('link-product', $redirectUrl);
+            $result = Service::make()->ebooks()
+                ->refreshEbook()
+                ->batch($postIds);
+            $redirectUrl = add_query_arg('updated-meta', $result['data']['refreshed'], $redirectUrl);
+        } catch (Exception $exception) {
+            error_log($exception->getMessage());
+            $redirectUrl = add_query_arg('updated-meta', 'fail', $redirectUrl);
         }
+
+        return $redirectUrl;
     }
 
     /**
-     * Links products and returns the response.
+     * Performs a mass linking of products for a set of eBooks.
      *
-     * This method is responsible for linking products. It first retrieves the post IDs from the request.
-     * If no post IDs are specified, it throws an exception. Then, it makes a service call to link the products,
-     * and batches them. The number of linked products is then checked.
-     * If the number of linked products is greater than 0, a success message is returned with the number of linked products.
-     * Otherwise, a message indicating that there are no products found to link is returned.
-     * The response from the service call and the message are then returned in a WP_REST_Response object.
+     * This method takes an array of post IDs and a redirect URL as parameters. It attempts to link the products
+     * for each post ID in the array by calling the `linkProduct` method on the WooCommerce service. The number of products
+     * successfully linked is then added to the redirect URL as a query parameter.
      *
-     * @param WP_REST_Request $request The request object, which should contain the post IDs to link.
-     * @return WP_REST_Response A WP_REST_Response object containing the response from the service call and the message.
-     * @throws \Exception If no post IDs are specified in the request.
+     * If an exception occurs during the linking process, the exception message is logged using the `error_log` function,
+     * and 'fail' is added to the redirect URL as a query parameter.
+     *
+     * @param array $postIds An array of post IDs for which to link the products.
+     * @param string $redirectUrl The URL to which the user should be redirected after the linking process is complete.
+     * @return string The redirect URL with the result of the linking process added as a query parameter.
+     * @throws Exception If an error occurs during the linking process.
      */
-    public function massLinkProducts(array $postIds): string
+    public function massLinkProducts(array $postIds, string $redirectUrl): string
     {
         try {
-            $response = Service::make()->wooCommerce()
+            $redirectUrl = remove_query_arg('updated-meta', $redirectUrl);
+            $result = Service::make()->wooCommerce()
                 ->linkProduct()
-                ->batch();
-
-            $message = $response['linked'] > 0
-                ? str_replace('%s', $response['linked'], esc_html__("Linked %s products successfully!", 'alfaomega-ebooks'))
-                : esc_html__('No products found to link', 'alfaomega-ebooks');
-
-            return new WP_REST_Response([
-                'status'  => 'success',
-                'data'    => $response,
-                'message' => $message,
-            ], 200);
-        } catch (\Exception $exception) {
-            return new WP_REST_Response([
-                'status'  => 'fail',
-                'message' => $exception->getMessage()
-            ], 400);
+                ->batch($postIds);
+            $redirectUrl = add_query_arg('link-product', $result['data']['linked'], $redirectUrl);
+        } catch (Exception $exception) {
+            error_log($exception->getMessage());
+            $redirectUrl = add_query_arg('link-product', 'fail', $redirectUrl);
         }
+        
+        return $redirectUrl;
     }
 
     /**
-     * Links ebooks and returns the response.
+     * Performs a mass linking of eBooks for a set of products.
      *
-     * This method is responsible for linking products. It first retrieves the post IDs from the request.
-     * If no post IDs are specified, it throws an exception. Then, it makes a service call to link the products,
-     * and batches them. The number of linked products is then checked.
-     * If the number of linked products is greater than 0, a success message is returned with the number of linked products.
-     * Otherwise, a message indicating that there are no products found to link is returned.
-     * The response from the service call and the message are then returned in a WP_REST_Response object.
+     * This method takes an array of post IDs and a redirect URL as parameters. It attempts to link the eBooks
+     * for each post ID in the array by calling the `linkEbook` method on the WooCommerce service. The number of eBooks
+     * successfully linked is then added to the redirect URL as a query parameter.
      *
-     * @param WP_REST_Request $request The request object, which should contain the post IDs to link.
-     * @return WP_REST_Response A WP_REST_Response object containing the response from the service call and the message.
-     * @throws \Exception If no post IDs are specified in the request.
+     * If an exception occurs during the linking process, the exception message is logged using the `error_log` function,
+     * and 'fail' is added to the redirect URL as a query parameter.
+     *
+     * @param array $postIds An array of post IDs for which to link the eBooks.
+     * @param string $redirectUrl The URL to which the user should be redirected after the linking process is complete.
+     * @return string The redirect URL with the result of the linking process added as a query parameter.
+     * @throws Exception If an error occurs during the linking process.
      */
-    public function massLinkEbooks(array $postIds): string
+    public function massLinkEbooks(array $postIds, string $redirectUrl): string
     {
         try {
-            $response = Service::make()->wooCommerce()
-                ->linkProduct()
-                ->batch();
+            //$redirectUrl = remove_query_arg('link-product', $redirectUrl);
+            $result = Service::make()->wooCommerce()
+                ->linkEbook()
+                ->batch($postIds);
 
-            $message = $response['linked'] > 0
-                ? str_replace('%s', $response['linked'], esc_html__("Linked %s products successfully!", 'alfaomega-ebooks'))
-                : esc_html__('No products found to link', 'alfaomega-ebooks');
-
-            return new WP_REST_Response([
-                'status'  => 'success',
-                'data'    => $response,
-                'message' => $message,
-            ], 200);
-        } catch (\Exception $exception) {
-            return new WP_REST_Response([
-                'status'  => 'fail',
-                'message' => $exception->getMessage()
-            ], 400);
+            $redirectUrl = add_query_arg('link-ebook', $result['data']['linked'], $redirectUrl);
+        } catch (Exception $exception) {
+            error_log($exception->getMessage());
+            $redirectUrl = add_query_arg('link-ebook', 'fail', $redirectUrl);
         }
+
+        return $redirectUrl;
     }
 }
