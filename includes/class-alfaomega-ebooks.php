ec0eaec12c28eefab12b3460a40018de54a52f6a
Livan A. Rodriguez <livan2r@gmail.com>
2024-08-20 16:54:26 -0400

    translation completed for admin tools

--- includes/class-alfaomega-ebooks.php
+++ includes/class-alfaomega-ebooks.php
@@ -309,11 +309,11 @@ class Alfaomega_Ebooks {
 
         add_submenu_page(
             'alfaomega_ebooks_admin',
-            esc_html__('Settings', 'alfaomega-ebooks'),
-            esc_html__('Settings', 'alfaomega-ebooks'),
+            esc_html__('Home', 'alfaomega-ebooks'),
+            esc_html__('Home', 'alfaomega-ebooks'),
             'install_plugins',
-            'alfaomega_ebooks_settings',
-            [$this, 'renderSettings'],
+            'alfaomega_ebooks_admin',
+            null,
             null
         );
 
@@ -354,6 +354,16 @@ class Alfaomega_Ebooks {
             'install_plugins',
             'alfaomega_ebooks_link',
             [$this, 'renderLink'],
+            null
+        );
+
+        add_submenu_page(
+            'alfaomega_ebooks_admin',
+            esc_html__('Settings', 'alfaomega-ebooks'),
+            esc_html__('Settings', 'alfaomega-ebooks'),
+            'install_plugins',
+            'alfaomega_ebooks_settings',
+            [$this, 'renderSettings'],
             null
         );
     }
--- views/alfaomega_ebook_home_page.php
+++ views/alfaomega_ebook_home_page.php
@@ -9,37 +9,37 @@
     <div class="alfaomega_ebooks-about-integrations">
         <div class="alfaomega_ebooks-feature feature-section col three-col">
             <div class="col alfaomega_ebooks-col alfaomega_ebooksPanelWrapper">
-                <div class="alfaomega_ebooksPanelHeading">Import new eBooks</div>
+                <div class="alfaomega_ebooksPanelHeading"><?php esc_html_e("Import new eBooks", 'alfaomega-ebooks') ?></div>
                 <div class="alfaomega_ebooksPanelBody">
                     <p class="alfaomega_ebooksPanelBody_text">
                         <?php esc_html_e("Pull new eBooks from the Publisher Panel, update the list of eBooks and search the relative product for the new ebooks to create a link using the Digital ISBN.", 'alfaomega-ebooks') ?>
                     </p>
                     <p class="alfaomega_ebooksLinkWrapper">
-                        <a href="?page=alfaomega_ebooks_import">Import eBooks</a>
+                        <a href="?page=alfaomega_ebooks_import"><?php esc_html_e("Import eBooks", 'alfaomega-ebooks') ?></a>
                     </p>
                 </div>
             </div>
 
             <div class="col alfaomega_ebooks-col alfaomega_ebooksPanelWrapper">
-                <div class="alfaomega_ebooksPanelHeading">Refresh eBooks</div>
+                <div class="alfaomega_ebooksPanelHeading"><?php esc_html_e("Refresh eBooks", 'alfaomega-ebooks') ?></div>
                 <div class="alfaomega_ebooksPanelBody">
                     <p class="alfaomega_ebooksPanelBody_text">
                         <?php esc_html_e("Resfresh the eBooks information pulling the current eBook data in Publisher Panel and search the relative product to create a link using the Digital ISBN.", 'alfaomega-ebooks') ?>
                     </p>
                     <p class="alfaomega_ebooksLinkWrapper">
-                        <a href="?page=alfaomega_ebooks_refresh">Refresh eBooks</a>
+                        <a href="?page=alfaomega_ebooks_refresh"><?php esc_html_e("Refresh eBooks", 'alfaomega-ebooks') ?></a>
                     </p>
                 </div>
             </div>
 
             <div class="col alfaomega_ebooks-col alfaomega_ebooksPanelWrapper">
-                <div class="alfaomega_ebooksPanelHeading">Link Products</div>
+                <div class="alfaomega_ebooksPanelHeading"><?php esc_html_e("Link Products", 'alfaomega-ebooks') ?></div>
                 <div class="alfaomega_ebooksPanelBody">
                     <p class="alfaomega_ebooksPanelBody_text">
                         <?php esc_html_e("Using the list of eBooks already imported search the relative product to create a link using the Digital ISBN.", 'alfaomega-ebooks') ?>
                     </p>
                     <p class="alfaomega_ebooksLinkWrapper">
-                        <a href="?page=alfaomega_ebooks_link">Link Products</a>
+                        <a href="?page=alfaomega_ebooks_link"><?php esc_html_e("Link Products", 'alfaomega-ebooks') ?></a>
                     </p>
                 </div>
             </div>
