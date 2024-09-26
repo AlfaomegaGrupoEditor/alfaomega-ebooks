8a7ec281c941709a4521ca662dee6b31c8501e19
Livan Rodriguez <livan2r@gmail.com>
2024-09-26 15:16:48 -0400

    showing the categories and z-index fixed

--- src/Resources/components/aoSidebar.vue
+++ src/Resources/components/aoSidebar.vue
@@ -1,81 +1,85 @@
 <script setup lang="ts">
-  import { ref, watch } from 'vue';
-  import { aoButton, aoAccessDetails } from '@/components';
-  import { BookType } from '@/types';
+    import {ref, watch} from 'vue';
+    import {aoButton, aoAccessDetails} from '@/components';
+    import {BookType} from '@/types';
 
-  const props = defineProps({
-    show: Boolean,
-    data: {type: Object as () => BookType, required: true}
-  });
+    const props = defineProps({
+        show: Boolean,
+        data: {type: Object as () => BookType, required: true}
+    });
 
-  const emit = defineEmits(['update:show']);
-  const show = ref(props.show);
-  const book = ref<BookType | null>(null);
+    const emit = defineEmits(['update:show']);
+    const show = ref(props.show);
+    const book = ref<BookType | null>(null);
 
-  const handleClose = () => {
-    emit('update:show', !show.value);
-  };
+    const handleClose = () => {
+        emit('update:show', !show.value);
+    };
 
-  watch(() => props.show, (newVal) => {
-    show.value = newVal;
-  });
+    watch(() => props.show, (newVal) => {
+        show.value = newVal;
+    });
 
-  watch(() => props.data, (newVal) => {
-    console.log(newVal);
-    book.value = newVal;
-  });
+    watch(() => props.data, (newVal) => {
+        console.log(newVal);
+        book.value = newVal;
+    });
 </script>
 
 <template>
-  <BOffcanvas
-      v-if="data"
-      v-model="show"
-      class="ao-sidebar"
-      placement="end"
-      style="z-index: 200000"
-      :hide-backdrop="false"
-      :header="false"
-      :shadow="true"
-      @hide="handleClose"
-  >
-    <template #title>
-      <span class="text-primary">
-        {{ book.title }}
-      </span>
-    </template>
+    <BOffcanvas
+        v-if="data"
+        v-model="show"
+        class="ao-sidebar"
+        placement="end"
+        style="z-index: 9999999991"
+        :hide-backdrop="false"
+        :header="false"
+        :shadow="true"
+        @hide="handleClose"
+    >
+        <template #title>
+            <span class="text-primary mt-2">
+                {{ book.title }}
+            </span>
+        </template>
 
-    <div class="mx-4">
-      <img
-          class="img-thumbnail"
-          :src="book.cover"
-          :alt="book.title"
-      />
-    </div>
+        <div class="mb-2">
+            <span class="fw-bold fs-7">{{ book.categories }}</span>
+        </div>
 
-    <div class="mt-4 d-flex justify-content-center">
-      <ao-button
-          icon="fa-file-pdf"
-          :caption="$t('download')"
-          :disabled="!book.download"
-          @click="() => console.log('click download')"
-      />
+        <div class="mx-4">
+            <img
+                class="img-thumbnail"
+                :src="book.cover"
+                :alt="book.title"
+            />
+        </div>
 
-      <ao-button
-          icon="fa-wifi"
-          :caption="$t('read_online')"
-          :disabled="!book.read"
-          @click="() => console.log('click read online')"
-      />
-    </div>
+        <div class="mt-4 d-flex justify-content-center">
+            <ao-button
+                icon="fa-file-pdf"
+                :caption="$t('download')"
+                :disabled="!book.download"
+                @click="() => console.log('click download')"
+            />
 
-    <ao-access-details
-        :type="book.type"
-        :status="book.status"
-        :added_at="book.addedAt"
-        :valid_until="book.validUntil"
-        :book_url="book.url"
-    />
-  </BOffcanvas>
+            <ao-button
+                icon="fa-wifi"
+                :caption="$t('read_online')"
+                :disabled="!book.read"
+                @click="() => console.log('click read online')"
+            />
+        </div>
+
+        <ao-access-details
+            :type="book.type"
+            :status="book.status"
+            :added_at="book.addedAt"
+            :valid_until="book.validUntil"
+            :book_url="book.url"
+        />
+    </BOffcanvas>
 </template>
 
 <style scoped>
