7d4e6daade960b2e9ec5f6db4fd67839081d5f93
Livan Rodriguez <livan2r@gmail.com>
2024-10-10 09:19:46 -0400

    dashboard route implemented

--- src/Resources/components/Dashboard.vue
+++ src/Resources/components/Dashboard.vue
@@ -15,6 +15,7 @@
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2 fs-6 fw-bold">{{ pageTitle }}</h1>
                     <BButton
+                        v-if="route.name !== 'dashboard'"
                         class="fw-bold"
                         variant="outline-primary"
                         size="sm"
