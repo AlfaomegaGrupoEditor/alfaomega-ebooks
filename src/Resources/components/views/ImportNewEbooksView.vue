52dcdcac4acd313084e926c9046e24cf901b595e
Livan Rodriguez <livan2r@gmail.com>
2024-10-15 15:35:53 -0400

    polling removed

--- src/Resources/components/views/ImportNewEbooksView.vue
+++ src/Resources/components/views/ImportNewEbooksView.vue
@@ -10,12 +10,9 @@ import {computed, onMounted, onUnmounted, ref} from 'vue';
     const {t} = useI18n();
     const processStore = useProcessStore();
     const importStatus = computed(() => processStore.getImportNewEbooks);
-    //const processing = computed(() => importStatus.value.status === 'processing');
     const modalName = 'import-ebooks-modal';
     const {show} = useModal(modalName);
     const intervalId = ref(null);
-    const poolTimeout = 60 * 1000;
-    const enablePolling = ref(false);
 
     const handleImport = (action) => {
         processStore.dispatchImportNewEbooks();
@@ -24,21 +21,6 @@ import {computed, onMounted, onUnmounted, ref} from 'vue';
             type: 'success'
         });
     };
-
-    /*onMounted(() => {
-        if (enablePolling.value) {
-            intervalId.value = setInterval(() => {
-                processStore.dispatchRetrieveQueueStatus('import-new-ebooks');
-            }, poolTimeout);
-        }
-
-        processStore.dispatchRetrieveQueueStatus('import-new-ebooks');
-    });
-
-    onUnmounted(() => {
-        clearInterval(intervalId.value);
-    });*/
-
 </script>
 
 <template>
